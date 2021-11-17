b13f03357829b485f2b0b52d6d3ff856672e36f4
TheLudwigGamer <theludwiggamer@gmail.com>
2021-11-17 09:07:34 +0000

    added grade calculator

--- javascript/test.js
+++ javascript/test.js
@@ -1,5 +1,22 @@
-document.write("spook-o-ween");
 var x = 69;
 var y = 420;
 
 alert("you're computer has virus #" + (y % x));
+
+var gradeString = prompt("Enter grade from 0-100")
+var gradeNumber = parseInt(gradeString);
+
+document.write("<b>Grade: ");
+
+if (gradeNumber < 39) {
+    document.write("Fail");
+} else if (gradeNumber < 54) {
+    document.write("Pass");
+} else if (gradeNumber < 74) {
+    document.write("Merit");
+} else if (gradeNumber < 100) {
+    document.write("Distinction");
+} else {
+    document.write("Cheater");
+}
+document.write("</b>");
