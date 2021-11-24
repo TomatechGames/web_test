a681b1fa72eac9ae23b4be8a865e96fd996dd9e4
TheLudwigGamer <theludwiggamer@gmail.com>
2021-11-24 10:08:48 +0000

    added form and form validation

--- javascript/test.js
+++ javascript/test.js
@@ -1,28 +1,13 @@
-var x = 69;
-var y = 420;
+function validateForm() {
+    var emailInput = document.getElementById("formEmail").value;
 
-var customText = "yee";
+    var firstAtIndex = emailInput.indexOf('@');
+    var firstDotIndex = emailInput.indexOf('.');
 
-alert("you're computer has virus #" + (y % x));
-
-var gradeString = prompt("Enter grade from 0-100")
-var gradeNumber = parseInt(gradeString);
-
-document.write("<b>Grade: ");
-
-if (gradeNumber < 39) {
-    document.write("Fail");
-} else if (gradeNumber < 54) {
-    document.write("Pass");
-} else if (gradeNumber < 74) {
-    document.write("Merit");
-} else if (gradeNumber < 100) {
-    document.write("Distinction");
-} else {
-    document.write("Cheater");
-}
-document.write("</b>");
-
-function bruh(bruhText) {
-    alert("bruh: " + bruhText);
+    //improve this, currently doesn't support full stops in the email name
+    if (firstAtIndex > 0 && firstDotIndex > firstAtIndex) {
+        document.getElementById("emailFeedback").innerHTML = emailInput.toUpperCase();
+    } else {
+        document.getElementById("emailFeedback").innerHTML = "invalid email";
+    }
 }
